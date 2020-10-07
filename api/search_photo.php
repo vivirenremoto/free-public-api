0ff2a6c534a496a7afb352753adcdc5102654712
vivirenremoto <gafeman@gmail.com>
2020-10-08 01:35:48 +0200

    Update search_photo.php

--- api/search_photo.php
+++ api/search_photo.php
@@ -1,6 +1,11 @@
 <?php
 
-$key = '11210836-792643a4560f2fbd817b3a812';
+// get your pixabay api key https://pixabay.com/api/docs/
+
+if (!$key) {
+  $key = '11210836-792643a4560f2fbd817b3a812';
+}
+
 $url = 'https://pixabay.com/api/?q=' . urlencode($value) . '&key=' . $key . '&image_type=photo';
 $data = json_decode( file_get_contents($url) );
 
