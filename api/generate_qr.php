ac4717c4c280d621289477e73b4c6ecaef0a0d3d
vivirenremoto <gafeman@gmail.com>
2020-10-07 23:40:23 +0200

    Create generate_qr.php

---
+++ api/generate_qr.php
@@ -0,0 +1,11 @@
+<?php
+
+header('Content-Type: image/png');
+
+$size = isset($_GET['size']) ? $_GET['size'] : 200;
+
+$url = 'https://chart.googleapis.com/chart?chs=' . $size . 'x' . $size . '&cht=qr&chl=' . urlencode($value);
+
+echo file_get_contents($url);
+
+exit();
