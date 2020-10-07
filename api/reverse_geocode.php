9174d21a79fd8cd36d32b908087982711b1b6d70
vivirenremoto <gafeman@gmail.com>
2020-10-08 01:56:22 +0200

    Create reverse_geocode.php

---
+++ api/reverse_geocode.php
@@ -0,0 +1,14 @@
+<?php
+
+// https://stackoverflow.com/questions/16187219/reverse-geocoding-not-working-google-maps-api-v2/56558812
+
+// reverse geocoding api - google api console
+
+$url = 'https://maps.google.com/maps/api/geocode/json?latlon=' . urlencode($value) . '&key=' . $key;
+$geocode = file_get_contents($url);
+
+$output = json_decode($geocode);
+
+if (isset($output->results[0])) {
+    $result = $output->results[0]->formatted_address;
+}
