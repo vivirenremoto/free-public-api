f13788b158a69a589e02b9c695ffa4c8f5da3692
miquel camps orteza <gafeman@gmail.com>
2020-10-08 02:59:37 +0200

    ADD cpc + search volume

---
+++ api/seo_keyword_cpc.php
@@ -0,0 +1,13 @@
+<?php
+
+$country = isset($_GET['country']) ? $_GET['country'] : 'es';
+
+$url = 'https://db2.keywordsur.fr/keyword_surfer_keywords?country=' . $country . '&keywords=%5B%22' . urlencode($value) . '%22%5D';
+$data = file_get_contents($url);
+$output = json_decode($data);
+
+if (isset($output->{$value}->search_volume)) {
+    $result = $output->{$value}->cpc;
+} else {
+    $result = 0;
+}
