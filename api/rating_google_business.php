cf6c318861b48b6084c76f5eab1317a6c1c1f217
miquel camps orteza <gafeman@gmail.com>
2020-10-10 15:39:46 +0200

    ADD google business api

---
+++ api/rating_google_business.php
@@ -0,0 +1,17 @@
+<?php
+
+// get place id - https://developers.google.com/places/web-service/place-id
+// docs https://developers.google.com/places/web-service/details
+
+if (!$key) {
+    die('error - api key (geocoding) and valid billing account are required on https://console.cloud.google.com/apis/');
+}
+
+$url = 'https://maps.googleapis.com/maps/api/place/details/json?place_id=' . $value . '&key=' . $key;
+
+$output = @file_get_contents($url);
+$json = json_decode($output);
+
+if (isset($json->result->rating)) {
+    $result = $json->result->rating;
+}
