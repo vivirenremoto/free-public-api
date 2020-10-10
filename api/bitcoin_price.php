337fbc63d7ad93856679cdc3af40ddc92c70636c
miquel camps orteza <gafeman@gmail.com>
2020-10-10 03:02:35 +0200

    ADD bitcoin price

---
+++ api/bitcoin_price.php
@@ -0,0 +1,11 @@
+<?php
+
+$currency = isset($_GET['currency']) ? $_GET['currency'] : 'EUR';
+
+$url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+$json = file_get_contents($url);
+$output = json_decode($json);
+
+if (isset($output->bpi->{$currency})) {
+    $result = $output->bpi->{$currency}->rate_float;
+}
