780e9b6d7f6c9c2b631ab71084365278036fbd01
miquel camps orteza <gafeman@gmail.com>
2020-10-10 02:07:29 +0200

    test

--- api/translate.php
+++ api/translate.php
@@ -5,11 +5,26 @@ $to = isset($_GET['to']) ? $_GET['to'] : false;
 
 if ($from && $to) {
 
-    $domain = 'https://free-public-api.herokuapp.com';
-    $content_url = '/decode_base64/?value=' . urlencode(base64_encode($value));
+    $url = 'https://www.bing.com/ttranslatev3';
 
-    $url = 'https://translate.googleusercontent.com/translate_c?depth=1&hl=' . $to . '&prev=search&pto=aue&rurl=translate.google.com&sl=' . $from . '&sp=nmt4&u=' . urlencode($content_url);
+    $post = array(
+        'fromLang' => $from,
+        'to' => $to,
+        'text' => $value,
+    );
 
-    $result = file_get_contents($url);
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post));
+    curl_setopt($curl, CURLOPT_REFERER, 'https://www.bing.com/translator');
+    curl_setopt($curl, CURLOPT_POST, 'POST');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    $data = curl_exec($curl);
+    curl_close($curl);
+
+    $output = json_decode($data);
+    if (isset($output[0]->translations[0]->text)) {
+        $result = $output[0]->translations[0]->text;
+    }
 
 }
