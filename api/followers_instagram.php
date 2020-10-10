b1568b085e2c7cb6161469e2213367bf90129ba0
miquel camps orteza <gafeman@gmail.com>
2020-10-10 15:09:36 +0200

    test

--- api/followers_instagram.php
+++ api/followers_instagram.php
@@ -1,7 +1,13 @@
 <?php
 
 $url = 'https://www.instagram.com/' . $value . '/?__a=1';
-echo $json = @file_get_contents($url);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+echo $json = curl_exec($ch);
+curl_close($ch);
+
 $output = json_decode($json);
 
 if (isset($output->graphql->user->edge_followed_by->count)) {
