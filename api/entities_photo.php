b10111053e11f6d10a15ddf7c7244d3e6af83e9d
miquel camps orteza <gafeman@gmail.com>
2020-10-11 03:38:07 +0200

    ADD extract entities from photo

---
+++ api/entities_photo.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!$key || !$secret) {
+    throw new Exception('api key and secret are required on https://imagga.com/');
+}
+
+$lang = isset($_GET['lang']) ? $_GET['lang'] : 'en';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://api.imagga.com/v2/tags?image_url=' . urlencode($value) . '&language=' . $lang);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_USERPWD, $key . ':' . $secret);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+$json_response = json_decode($response);
+
+if (isset($json_response->result->tags[0])) {
+
+    $result = '';
+    foreach ($json_response->result->tags as $k => $tag) {
+        if ($k) {
+            $result .= ';';
+        }
+
+        $result .= $tag->tag->{$lang};
+    }
+}
--- index.php
+++ index.php
@@ -22,6 +22,7 @@ $value = isset($_GET['value']) ? $_GET['value'] : null;
 $callback = isset($_GET['callback']) ? $_GET['callback'] : null;
 $format = isset($_GET['format']) ? $_GET['format'] : 'text';
 $key = isset($_GET['key']) ? $_GET['key'] : null;
+$secret = isset($_GET['secret']) ? $_GET['secret'] : null;
 $default = isset($_GET['default']) ? $_GET['default'] : false;
 $result = false;
 
