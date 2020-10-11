53ef6d9903e67ae823af8a8f8d125bcae4b45468
miquel camps orteza <gafeman@gmail.com>
2020-10-11 05:11:49 +0200

    ADD validate nude photo

---
+++ api/validate_nude_photo.php
@@ -0,0 +1,29 @@
+<?php
+
+// https://stackoverflow.com/questions/58664912/problem-with-using-curl-in-php-for-deepai-api
+
+if (!$key) {
+    throw new Exception('api key is required on https://deepai.org/');
+}
+
+$url = "https://api.deepai.org/api/nsfw-detector";
+$ch = curl_init($url);
+$data_string = array('image' => $value);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Api-Key: ' . $key));
+$output = curl_exec($ch);
+$json = json_decode($output);
+
+if (isset($json->output->detections)) {
+
+    if (isset($json->output->detections[0])) {
+        $result = $json->output->detections[0]->confidence >= 0.8;
+    } else {
+        $result = false;
+    }
+
+} else if (isset($json->status)) {
+    throw new Exception($json->status);
+}
