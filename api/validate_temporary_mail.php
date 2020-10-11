b616f87c67b7f63c04931f74bd2bdde1ea2b614b
miquel camps orteza <gafeman@gmail.com>
2020-10-11 04:14:17 +0200

    ADD temporary email api

---
+++ api/validate_temporary_mail.php
@@ -0,0 +1,13 @@
+<?php
+
+// https://gist.github.com/duongthanhthai/6d8ec929889db2ad944abcd0742ab8fc
+
+if (strstr($value, '@')) {
+    $t_email = explode('@', $value);
+    $domain = end($t_email);
+
+    $path = __DIR__ . '/sources/temporary_mail_providers.txt';
+    $items = explode("\r\n", file_get_contents($path));
+
+    $result = in_array($domain, $items);
+}
