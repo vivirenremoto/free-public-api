12dd5342e8fad4d39e1f2e9432e2267b90f319ad
miquel camps orteza <gafeman@gmail.com>
2020-12-05 15:06:37 +0100

    FIX doc

--- static/main.js
+++ static/main.js
@@ -98,6 +98,8 @@ function showModal(obj) {
                 $('#params tbody').append('<tr><td width="25%">callback</td><td width="75%">optional</td></tr>');
             }
 
+            $('#params tbody').append('<tr><td width="25%">spreadsheet</td><td width="75%">requiered value if you are using google spreadsheets</td></tr>');
+
         }
     } else {
         $('#params').hide();
